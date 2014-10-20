2592d1e90cdace99ae84951a8862839edd1926d3
Daniel Cox <danzabian@gmail.com>
2014-10-20 07:41:23 +0100

    Added base of ORM config

---
+++ src/ORM/Model.php
@@ -0,0 +1,19 @@
+<?php namespace Danzabar\Config\ORM;
+
+use Danzabar\Config\ORM\ModelInterface;
+
+
+/**
+ * A configuration Model used to create an ORM out of config arrays
+ *
+ * @package Config
+ * @subpackage ORM
+ * @author Dan Cox
+ */
+class Model implements ModelInterface
+{
+
+
+
+
+} // END class Model
